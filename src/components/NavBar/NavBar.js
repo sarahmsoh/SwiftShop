3b429a21cce864dde7a95ce0df4243ccd854a776
sarahmsoh <sarahmuriithi523@gmail.com>
2024-11-16 11:45:24 +0300

    added

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,34 +1,26 @@
 import React from 'react';
 import styles from './NavBar.module.css';
-import { useNavigate, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-
-function NavBar({ onLogout }) {
-  const navigate = useNavigate();
-
-  const handleLogout = () => {
-    if (typeof onLogout === 'function') {
-      onLogout(); 
-      navigate('/login'); 
-    } else {
-      console.error('onLogout is not a function');
-    }
-  };
-
+function NavBar({ onLogout, onContactClick, onCartClick }) {
   return (
     <nav className={styles.navbar}>
-
       <div className={styles.navLinks}>
-      <Link to="/home" className={styles.navLink}>Home</Link>
-      <Link to="/contact" className={styles.navLink}>Contact</Link>
-      <Link to="/login" className={styles.navLink}>login</Link>
+        <Link to="/home" className={styles.navLink}>
+          Home
+        </Link>
+        <Link onClick={onContactClick} className={styles.navLink}>
+          Contact
+        </Link>
+        <Link onClick={onCartClick} className={styles.navLink}>
+          Cart
+        </Link>
       </div>
-
-      <button className={styles.logoutButton} onClick={handleLogout}>Logout</button>
+      <button className={styles.logoutButton} onClick={onLogout}>
+        Logout
+      </button>
     </nav>
   );
 }
-
-
 
 export default NavBar;
